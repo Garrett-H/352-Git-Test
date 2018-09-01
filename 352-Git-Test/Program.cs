88d08c6b6bde280861d8905abf8ddbec0f08ca0a
Garrett Hay <garlhay@ut.utm.edu>
2018-09-01 18:03:40 -0500

    Made game able to use user-given ranges

--- 352-Git-Test/Program.cs
+++ 352-Git-Test/Program.cs
@@ -13,10 +13,17 @@ namespace _352_Git_Test
             Console.WriteLine("Hello World");
             //random number guessing game
             Random rnd = new Random();
-            Console.WriteLine("Starting guessing game.\nNumber is between 0-100");
-            int ans = rnd.Next(100);
+            Console.WriteLine("Starting guessing game.\nWhat is the range?: ");
+            //alloting ranges
+            Console.Write("Lowest bound: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Highest bound: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            //creating random number
+            int ans = rnd.Next(first, second);
             Console.Write("Guess the number: ");
             int guess = Convert.ToInt32(Console.ReadLine());
+            //loops if incorrect
             while(guess != ans)
             {
                 if(guess < ans)
