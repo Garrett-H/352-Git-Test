cb162b4ee39a14776f7cce407aa58b09dce2fcd1
Garrett Hay <garlhay@ut.utm.edu>
2018-09-01 17:51:43 -0500

    Name generator created.

--- 352-Git-Test/Program.cs
+++ 352-Git-Test/Program.cs
@@ -11,6 +11,24 @@ namespace _352_Git_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            //random number guessing game
+            Random rnd = new Random();
+            Console.WriteLine("Starting guessing game.\nNumber is between 0-100");
+            int ans = rnd.Next(100);
+            Console.Write("Guess the number: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            while(guess != ans)
+            {
+                if(guess < ans)
+                    Console.WriteLine("Too Low");
+                else
+                    Console.WriteLine("Too High");
+                Console.Write("Guess again: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n You got the right number!");
+
+            Console.WriteLine("\nPress Enter to exit");
             Console.Read();
         }
     }
